0d0ad7e48621a99a6f5b67672bb7d42f4fd93248
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2024-11-10 21:20:18 +0800

    Feat: added scenery lighting
    Added soft 2d lights to scenery (vines, spider web 2, love letter, end scene and spider jump section), and both glow worm butts for atmospheric lighting when full glow is switched off

--- Glowmance/Assets/Scripts/playerMovement.cs
+++ Glowmance/Assets/Scripts/playerMovement.cs
@@ -2,40 +2,36 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public Rigidbody2D _rb2D;
-    public GameObject tailGlow;
-    public GameObject buttOff;
-    public GameObject _loveLetter;
+    GameObject tailGlow;
+    GameObject loveLetter;
     public Animator luminaMove;
-    float moveSpeed;
+    public float moveSpeed;
     public float jumpForce;
     bool isJumping;
     float moveHorizontal;
     float moveVertical;
     public bool isLitAf;
-    public bool sceneDark;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb2D = gameObject.GetComponent<Rigidbody2D>();
-        _loveLetter = GameObject.FindWithTag("loveLetter");
-        moveSpeed = 3f;
-        // jumpForce = 10f;
-        isJumping = false;
+        luminaMove = GetComponent<Animator>();
+        loveLetter = GameObject.FindWithTag("loveLetter");
         tailGlow = GameObject.FindWithTag("TailGlow");
         tailGlow.SetActive(true);
-        buttOff = GameObject.FindWithTag("ButtOff");
-        buttOff.SetActive(false);
         isLitAf = true;
-        sceneDark = false;
-        luminaMove = GetComponent<Animator>();
+        isJumping = false;
+        moveSpeed = 3f;
+        // jumpForce = 10f;
     }
+
     // Update is called once per frame
     void Update()
     {
         // Moving left and right - below code will give a number of -1 for left, 0 for still, 1 for right
         moveHorizontal = Input.GetAxisRaw("Horizontal");
-        // Moving up and down`
+        // Moving up and down
         moveVertical = Input.GetAxisRaw("Vertical");
         // _rb2D.AddForce(xInput * speed, 0, zInput * speed);
         if (Input.GetKeyUp(KeyCode.F))
@@ -44,13 +40,11 @@ public class playerMovement : MonoBehaviour
             if (isLitAf == true)
             {
                 tailGlow.SetActive(false);
-                buttOff.SetActive(true);
                 isLitAf = false;
             }
             else
             {
                 tailGlow.SetActive(true);
-                buttOff.SetActive(false);
                 isLitAf = true;
             }
         }
