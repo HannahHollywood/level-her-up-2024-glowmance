b4881e2b4925742b508f8fd44e7a5c0fb022df73
hannahbeth <h.hannahbecker@gmail.com>
2024-11-10 09:51:08 +0800

    Player now jumps and stays on the platforms

---
+++ Glowmance/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody2D _rb2D;
+    float moveSpeed;
+    public float jumpForce;
+    bool isJumping;
+    float moveHorizontal;
+    float moveVertical;
+    public bool isLitAf;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb2D = gameObject.GetComponent<Rigidbody2D>();
+        moveSpeed = 3f;
+        // jumpForce = 10f;
+        isJumping = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Moving left and right - below code will give a number of -1 for left, 0 for still, 1 for right
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        // Moving up and down`
+        moveVertical = Input.GetAxisRaw("Vertical");
+        // _rb2D.AddForce(xInput * speed, 0, zInput * speed);
+    }
+    void FixedUpdate()
+    {
+        if (moveHorizontal > 0.1f || moveHorizontal < -0.1f)
+        {
+            // vector2(left/right, y-axis), and then forcemode (some will add Time.Deltatime instead, but this is applied by default to ForceMode)
+            _rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+        if (!isJumping && moveVertical > 0.1f)
+        {
+            // jump
+            _rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = false;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = true;
+        }
+    }
+}
