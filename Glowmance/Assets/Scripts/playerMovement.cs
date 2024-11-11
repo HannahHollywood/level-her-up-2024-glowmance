fa0816aa85404c0eecfd4f6245ff9778cf07562d
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2024-11-11 19:23:21 +0800

    feat: added all animation clips
    Including turn left and right for lumina

--- Glowmance/Assets/Scripts/playerMovement.cs
+++ Glowmance/Assets/Scripts/playerMovement.cs
@@ -2,27 +2,31 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public Rigidbody2D _rb2D;
+    public SpriteRenderer _sr;
     GameObject tailGlow;
     GameObject loveLetter;
-    public Animator luminaMove;
+    public Animator animator;
     public float moveSpeed;
     public float jumpForce;
     bool isJumping;
     float moveHorizontal;
     float moveVertical;
     public bool isLitAf;
+    private Vector2 startPos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb2D = gameObject.GetComponent<Rigidbody2D>();
-        luminaMove = GetComponent<Animator>();
+        _sr = gameObject.GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         loveLetter = GameObject.FindWithTag("loveLetter");
         tailGlow = GameObject.FindWithTag("TailGlow");
         tailGlow.SetActive(true);
         isLitAf = true;
         isJumping = false;
         moveSpeed = 3f;
+        startPos = gameObject.transform.position;
         // jumpForce = 10f;
     }
 
@@ -49,6 +53,7 @@ public class playerMovement : MonoBehaviour
             }
         }
 
+
     }
 
     void FixedUpdate()
@@ -57,36 +62,65 @@ public class playerMovement : MonoBehaviour
         {
             // vector2(left/right, y-axis), and then forcemode (some will add Time.Deltatime instead, but this is applied by default to ForceMode)
             _rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+
+            // If looking left, flip Lumina left, else if right, flip right
+            if (moveHorizontal < -0.1f)
+            {
+                _sr.flipX = false;
+            }
+            else
+            {
+                _sr.flipX = true;
+            }
         }
+
         if (!isJumping && moveVertical > 0.1f)
         {
             // jump
             _rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
-            luminaMove.SetBool("Jump", true);
+            // This sets the animator transition condition to active
+            animator.SetBool("Jump", true);
         }
         else
         {
-            luminaMove.SetBool("Jump", false);
+            // This sets the animator transition condition to inactive
+            animator.SetBool("Jump", false);
         }
         // Debug.Log(_rb2D.linearVelocityX);
-        luminaMove.SetFloat("Speed", Mathf.Abs(_rb2D.linearVelocityX));
+        // This sets the animator transition condition speed to match Lumina velocity
+        animator.SetFloat("Speed", Mathf.Abs(_rb2D.linearVelocityX));
 
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
+        // When on ground, Lumina 'not jumping'
         if (collision.gameObject.tag == "Platform")
         {
             isJumping = false;
         }
 
+        // Lumina picks up love letter
         if (collision.gameObject.tag == "loveLetter")
         {
             // letter object is destroyed
             Destroy(collision.gameObject);
         }
+
+        // Spider kills Lumina
+        if (collision.gameObject.tag == "spider")
+        {
+            // letter object is destroyed
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag == "fallDeath")
+        {
+            gameObject.transform.position = startPos;
+        }
     }
     void OnTriggerExit2D(Collider2D collision)
     {
+        // When not on ground, Lumina 'is jumping'
         if (collision.gameObject.tag == "Platform")
         {
             isJumping = true;
